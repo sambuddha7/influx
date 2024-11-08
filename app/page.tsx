c873e3eea75a789addf0f6e785103c0db3065040
Sambuddha Biswas <sambuddhabiswas@MacBook-Air-8.local>
2024-11-08 01:16:11 -0600

    Initial commit

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import Image from "next/image";
+import { ThreeDCardDemo } from "@/components/test";
+import { HeroHighlightDemo } from "@/components/heroinflux";
+
+export default function Home() {
+  return (
+    <div>
+
+        <HeroHighlightDemo />
+    </div>
+  );
+}
