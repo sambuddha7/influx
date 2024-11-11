5abfc18d63fcdf10747544763eb04ca015de5699
sambuddha7 <sambuddha2019@gmail.com>
2024-11-10 19:42:41 -0600

    add link

--- components/navbar.tsx
+++ components/navbar.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 
 import ThemeSwitch from './ThemeSwitch';
+import Link from 'next/link';
+
 
 const Navbar = () => {
   return (
@@ -25,7 +27,7 @@ const Navbar = () => {
     // </nav>
     <div className="navbar bg-base-100 shadow-sm sticky top-0 z-50 bg-inherit">
   <div className="flex-1">
-    <a href="/"className="btn btn-ghost text-3xl">Influx</a>
+    <Link href="/"className="btn btn-ghost text-3xl">Influx</Link>
   </div>
   <div className="flex-none">
     <button className="btn btn-square btn-ghost">
