f24c62ead62d55a675b58c75a43d02d181312a6c
AJ <ajindal3@illinois.edu>
2025-01-23 00:13:13 -0500

    esteban fkn snake

--- app/page.tsx
+++ app/page.tsx
@@ -22,7 +22,7 @@ export default function Home() {
         {/* features */}
 
         <FeaturesSectionDemo />
-        <FeaturesSectionDemo1 />
+        {/* <FeaturesSectionDemo1 /> */}
 
         {/* why reddit */}
         <h2 className="bg-clip-text text-transparent text-center bg-gradient-to-b from-neutral-900 to-neutral-700 dark:from-neutral-600 dark:to-white text-2xl md:text-4xl lg:text-7xl font-sans py-2 md:py-10 relative z-20 font-bold tracking-tight">
